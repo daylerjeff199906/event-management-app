7fb31aa787ed586c287ed95d9cf64c6e4269a555
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-08-23 16:57:44 -0500

    feat: add loading skeleton components for improved UI loading experience

--- src/app/(dashboard)/dashboard/profile/loading.tsx
+++ src/app/(dashboard)/dashboard/profile/loading.tsx
@@ -21,7 +21,7 @@ const ImageSkeleton = () => (
 export default function Loading() {
   return (
     <div
-      className={`max-w-4xl mx-auto p-4 sm:px-6 lg:px-8 `}
+      className={`max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 `}
       aria-busy="true"
       aria-live="polite"
       aria-label="Cargando perfil de usuario"
