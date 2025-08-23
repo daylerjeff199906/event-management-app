916dfbda7d315ccd8dddb3436957b5e24ed7f6e0
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-08-23 18:20:02 -0500

    feat: update app configuration and add metadata for improved SEO and user experience

--- src/data/config.app.ts
+++ src/data/config.app.ts
@@ -1,5 +1,8 @@
 export const APP_CONFIG = {
-  appName: 'Mi Aplicación', // Nombre de la aplicación
+  // Nombre de la aplicación
+  appName: 'VamoYa',
+  description:
+    'VamoYa es la plataforma líder para crear, gestionar y potenciar eventos exitosos. Impulsa tu marca, conecta con tu audiencia y lleva tus experiencias al siguiente nivel.',
   logos: {
     small: '/assets/logo-small.png', // Ruta del logo pequeño
     large: '/assets/logo-large.png' // Ruta del logo grande
---
+++ src/data/config.meta.ts
@@ -0,0 +1,26 @@
+import { Metadata } from 'next'
+
+export const metadataBase: Metadata = {
+  title: 'VamoYa',
+  description:
+    'Descubre qué habrá en tu ciudad y disfruta de los mejores eventos con VamoYa. Encuentra, registra y vive experiencias únicas cerca de ti.',
+  openGraph: {
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/coniap-iiap.appspot.com/o/EPG%2Fsigae-background.webp?alt=media&token=16abc6dc-337b-497f-a605-d92d7e61899f',
+        width: 1000,
+        height: 630,
+        alt: 'Template EPG - UNAP'
+      }
+    ]
+  }
+}
+
+export const metadataTemplate: Metadata = {
+  title: {
+    default: 'Eventos | VamoYa',
+    template: '%s | Eventos | VamoYa'
+  },
+  description:
+    'Explora y descubre eventos únicos cerca de ti con VamoYa. Encuentra información detallada de cada evento y vive nuevas experiencias.'
+}
