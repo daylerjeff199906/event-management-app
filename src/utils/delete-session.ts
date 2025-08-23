98125df737b1237651d9d5a6cba93df514f5b005
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-08-23 01:07:31 -0500

    feat: implement dashboard menu structure, enhance onboarding steps with custom toast notifications, and improve button styling

--- src/utils/delete-session.ts
+++ src/utils/delete-session.ts
@@ -1,8 +1,11 @@
 'use server'
 const APP_NAME = process.env.APP_NAME
+import { getSupabase } from '@/services/core.supabase'
 import { cookies } from 'next/headers'
 
 export async function deleteSession() {
   const cookieStore = await cookies()
+  const supabase = await getSupabase()
+  await supabase.auth.signOut()
   cookieStore.delete(`${APP_NAME}_session`)
 }
