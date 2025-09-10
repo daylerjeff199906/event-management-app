bd7969ebcfa80c93d052f4df020d2e9583333ef6
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-09-10 09:35:39 -0500

    feat: mejorar la página de eventos de la institución con enlaces y diseño actualizado

--- src/app/(dashboard)/dashboard/(institution)/institutions/[slug]/page.tsx
+++ src/app/(dashboard)/dashboard/(institution)/institutions/[slug]/page.tsx
@@ -1,5 +1,7 @@
+import { APP_URL } from '@/data/config-app-url'
 import { InstitutionEventsPage } from '@/modules/events/page'
 import { fetchEventsByInstitution } from '@/services/events.services'
+import Link from 'next/link'
 
 interface PageProps {
   params: {
@@ -14,12 +16,24 @@ export default async function Page(props: PageProps) {
   const id_institution = String(slug)
 
   const response = await fetchEventsByInstitution({
-    institution_id: id_institution
+    institution_id: id_institution,
+    pageSize: 4
   })
 
   return (
-    <>
+    <div className="flex flex-col gap-6">
+      <div className="flex items-center justify-between md:gap-0 gap-4">
+        <h3 className="text-lg font-semibold text-foreground">
+          Últimos eventos creados por la institución
+        </h3>
+        <Link
+          className="text-sm text-primary hover:underline"
+          href={APP_URL.DASHBOARD.INSTITUTION.EVENTS(slug)}
+        >
+          Ver todos los eventos
+        </Link>
+      </div>
       <InstitutionEventsPage eventsList={response.data?.data || []} />
-    </>
+    </div>
   )
 }
