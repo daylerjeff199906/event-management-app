4e12d0b12c67446356a4056bee653752645e260a
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-09-23 20:24:54 -0500

    refactor: clean up conditional rendering logic in Footer component

--- src/modules/dashboard/components/index.ts
+++ src/modules/dashboard/components/index.ts
@@ -1,5 +1,6 @@
 import AvatarUploadPage from './image-profile-upload'
 import { InterestForm } from './interest-form'
 import { SettingsEditor } from './settings-editor'
+import { AddUserModal } from './add-user-modal'
 
-export { AvatarUploadPage, InterestForm, SettingsEditor }
+export { AvatarUploadPage, InterestForm, SettingsEditor, AddUserModal }
