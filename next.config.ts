6c3589a0c8869cd85429514e5d1baf6d91e94323
unknown <UNAP@economicoslap05.postgradounap.edu.pe>
2025-11-27 11:59:47 -0500

    fix: corregir el hostname en la configuración de imágenes para incluir 'www'

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "peru.travel",
+        hostname: "www.peru.travel",
         port: "",
       },
     ],
