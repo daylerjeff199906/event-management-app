7c2cde75dfb5f2c6c5e93de445d33f337b6a6340
unknown <UNAP@economicoslap05.postgradounap.edu.pe>
2025-11-27 10:44:08 -0500

    feat: actualizar configuración de imágenes para usar remotePatterns en lugar de dominios

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */ 
   images: {
-    domains: ['www.peru.travel'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "peru.travel",
+        port: "",
+      },
+    ],
     
   },
 };
