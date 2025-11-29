7a86ebcbbc5a40b9eb574a67f6c182e5f6401171
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-11-28 23:58:42 -0500

    feat: enhance user management and UI updates in various components

---
+++ src/app/(dashboard)/organizations/event/[event]/layout.tsx
@@ -0,0 +1,75 @@
+import { APP_URL } from '@/data/config-app-url'
+import { EventManageLayout } from '@/modules/events'
+import { Params } from '@/types'
+
+interface LayoutProps {
+  children: React.ReactNode
+  params: Params
+}
+
+export default async function Layout(props: LayoutProps) {
+  const { children } = props
+  const params = await props.params
+  const institutionId = params?.slug?.toString()
+  const eventId = params?.event?.toString()
+
+  return (
+    <EventManageLayout
+      backLabel="Volver a eventos"
+      menuItems={[
+        {
+          id: APP_URL.ORGANIZATION.INSTITUTION.EDIT_EVENT(
+            institutionId || '',
+            eventId || ''
+          ),
+          label: 'Detalles',
+          description: 'Información básica del evento',
+          href: APP_URL.ORGANIZATION.INSTITUTION.EDIT_EVENT(
+            institutionId || '',
+            eventId || ''
+          )
+        },
+        {
+          id: APP_URL.ORGANIZATION.INSTITUTION.EVENT_INFO(
+            institutionId || '',
+            eventId || ''
+          ),
+          label: 'Más información',
+          description: 'Información adicional del evento',
+          href: APP_URL.ORGANIZATION.INSTITUTION.EVENT_INFO(
+            institutionId || '',
+            eventId || ''
+          )
+        },
+        {
+          id: APP_URL.ORGANIZATION.INSTITUTION.ADD_SCHEDULE(
+            institutionId || '',
+            eventId || ''
+          ),
+          label: 'Agregar agenda',
+          description: 'Agregar un nuevo horario para el evento',
+          href: APP_URL.ORGANIZATION.INSTITUTION.ADD_SCHEDULE(
+            institutionId || '',
+            eventId || ''
+          )
+        },
+        {
+          id: APP_URL.ORGANIZATION.INSTITUTION.ADD_TICKET(
+            institutionId || '',
+            eventId || ''
+          ),
+          label: 'Agregar entradas',
+          description: 'Agregar entradas para el evento',
+          href: APP_URL.ORGANIZATION.INSTITUTION.ADD_TICKET(
+            institutionId || '',
+            eventId || ''
+          ),
+          disabled: true
+        }
+      ]}
+      urlBack={APP_URL.ORGANIZATION.INSTITUTION.EVENTS(institutionId || '')}
+    >
+      {children}
+    </EventManageLayout>
+  )
+}
